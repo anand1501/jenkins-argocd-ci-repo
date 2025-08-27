2c6396b429d38e2a167f5f02054d33806a96cf47
anand1501 <anand31798@gmail.com>
2025-08-27 16:56:14 +0530

    change in app code

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def lw():
-    return("Testing the the application for v6...")
+def app():
+    return("2nd Testing the the application...")
